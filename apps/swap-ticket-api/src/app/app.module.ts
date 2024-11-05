306dac8a399d92a19c54d8227e74df2d947664fb
Sybrand Bos <snjw.bos@student.avans.nl>
2024-11-05 20:53:21 +0100

    Workflow for CI/CD

--- apps/swap-ticket-api/src/app/app.module.ts
+++ apps/swap-ticket-api/src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URL),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
